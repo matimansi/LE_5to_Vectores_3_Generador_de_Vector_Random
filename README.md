91016e7a845307bdfbeb982632f09d3714b472ea
Matias Mansilla <mmansilla@etrr.edu.ar>
2022-10-28 21:50:14 -0300

    README update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # LE_5to_Vectores_3_Generador_de_Vector_Random
  
+Consigna:
+    Desarrollar un programa que pida un valor máximo y uno mínimo por
+	pantalla para poder inicializar un vector de 10 elementos con valores
+	random en ese rango. Imprimir el vector. 
+
+    a. *buscar el internet el uso de la función rand()
+
+Investigacion:
+	https://www.chuidiang.org/clinux/funciones/rand.php#:~:text=En%20C%2C%20para%20obtener%20n%C3%BAmeros,como%20de%202%20mil%20millones
--- Vector_Random.c
+++ Vector_Random.c
@@ -1,16 +1,4 @@
 
-/*
-    Desarrollar un programa que pida un valor máximo y uno mínimo por
-	pantalla para poder inicializar un vector de 10 elementos con valores
-	random en ese rango. Imprimir el vector. 
-
-    a. *buscar el internet el uso de la función rand()
-
-	https://www.chuidiang.org/clinux/funciones/rand.php#:~:text=En%20C%2C%20para%20obtener%20n%C3%BAmeros,como%20de%202%20mil%20millones
-
-	>>> TERMINADO <<<
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
